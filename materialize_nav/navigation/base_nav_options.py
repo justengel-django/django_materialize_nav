ebcc2f25caa0eaa1fc03b4734be5e308c51d733c
jengel <jengel@sealandaire.com>
2018-03-27 15:21:58 -0400

    Fixed request context issue. Request will always be in the context now.

--- materialize_nav/navigation/base_nav_options.py
+++ materialize_nav/navigation/base_nav_options.py
@@ -4,20 +4,6 @@ from dynamicmethod import dynamicmethod
 __all__ = ['BaseNavOptions']
 
 
-class FakeDict(object):
-    def __init__(self, *args, **kwargs):
-        self.d = dict(*args, **kwargs)
-
-    def __getitem__(self, item):
-        return self.d[item]
-
-    def __setitem__(self, item, value):
-        self.d[item] = value
-
-    def __getattr__(self, item):
-        return self.d[item]
-
-
 class BaseNavOptions(object):
     @dynamicmethod
     def get_context(self, request, context=None, *, notification=None, **kwargs):
@@ -27,8 +13,9 @@ class BaseNavOptions(object):
         context['previous_page'] = request.META.get("HTTP_REFERER", '/')
         context['notification'] = request.GET.get("notification", notification)
 
-        # Fake the request.user
-        context['request'] = FakeDict(user=request.user, path=request.path, get_full_path=request.get_full_path)
+        # Force the request to be in the context
+        if "request" not in context:
+            context['request'] = request
 
         # Notify that the nav context was loaded
         request.nav_context_loaded = True
