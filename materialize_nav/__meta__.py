35349041e41defe599f959047d9e3f6e4bed38c2
justengel <jtengel08@gmail.com>
2021-04-26 09:17:18 -0400

    Updated to version 1.2.0

--- materialize_nav/__meta__.py
+++ materialize_nav/__meta__.py
@@ -1,5 +1,5 @@
 name = 'django_materialize_nav'
-version = '1.1.3'
+version = '1.2.0'
 description = 'Django materialize css support.'
 url = 'https://github.com/justengel-django/django_materialize_nav'
 author = 'Justin Engel'
